0af84cd8a23f3296e159b1bb4e7812b583347e0d
guanguans <ityaozm@gmail.com>
2021-02-23 18:21:51 +0800

    Wip

--- src/helpers.php
+++ src/helpers.php
@@ -7,3 +7,34 @@
  *
  * This source file is subject to the MIT license that is bundled.
  */
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+if (! function_exists('configure_options')) {
+    /**
+     * Configuration options.
+     *
+     * @param \Closure $closure
+     * @param null     $object
+     *
+     * @return array
+     */
+    function configure_options(array $options, Closure $closure, $object = null)
+    {
+        $resolver = new OptionsResolver();
+        $closure($resolver);
+        if ($object instanceof Closure) {
+            return $object()->options = $resolver->resolve($options);
+        }
+        if (is_object($object)) {
+            return $object->options = $resolver->resolve($options);
+        }
+        if (is_string($object) && class_exists($object)) {
+            $object = new $object();
+
+            return $object->options = $resolver->resolve($options);
+        }
+
+        return $resolver->resolve($options);
+    }
+}
--- tests/Heplers.php
+++ tests/Heplers.php
@@ -10,10 +10,81 @@
 
 namespace Coolephp\OptionsResolver\Tests;
 
+use Symfony\Component\OptionsResolver\Exception\InvalidOptionsException;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
 class Heplers extends TestCase
 {
-    public function testTest()
+    public function testConfigureOptions()
     {
-        $this->assertTrue(true);
+        $options = [
+            'host' => 'smtp.example.org',
+            'username' => 'user',
+            'password' => 'password',
+            'port' => 25,
+        ];
+        $emailStub = new EmailStub($options);
+        $this->assertEquals($options, $emailStub->getOptions());
+
+        $options = configure_options($options, function (OptionsResolver $resolver) {
+            $resolver->setDefaults([
+                'host' => 'smtp.example.org',
+                'username' => 'user',
+                'password' => 'password',
+                'port' => 25,
+            ]);
+            $resolver->setRequired(['host', 'username', 'password', 'port']);
+            $resolver->setAllowedTypes('host', 'string');
+            $resolver->setAllowedTypes('username', 'string');
+            $resolver->setAllowedTypes('password', 'string');
+            $resolver->setAllowedTypes('port', 'int');
+        }, $this);
+
+        $this->assertEquals($options, $this->options);
+    }
+
+    public function testConfigureOptionsInvalidOptionsException()
+    {
+        $options = [
+            'host' => 'smtp.example.org',
+            'username' => 'user',
+            'password' => 'password',
+            'port' => '25',
+        ];
+        new EmailStub($options);
+        $this->expectException(InvalidOptionsException::class);
+    }
+}
+
+class EmailStub
+{
+    private $options;
+
+    /**
+     * @return mixed
+     */
+    public function getOptions()
+    {
+        return $this->options;
+    }
+
+    /**
+     * Email constructor.
+     */
+    public function __construct(array $options = [])
+    {
+        configure_options($options, function (OptionsResolver $resolver) {
+            $resolver->setDefaults([
+                'host' => 'smtp.example.org',
+                'username' => 'user',
+                'password' => 'password',
+                'port' => 25,
+            ]);
+            $resolver->setRequired(['host', 'username', 'password', 'port']);
+            $resolver->setAllowedTypes('host', 'string');
+            $resolver->setAllowedTypes('username', 'string');
+            $resolver->setAllowedTypes('password', 'string');
+            $resolver->setAllowedTypes('port', 'int');
+        }, $this);
     }
 }
